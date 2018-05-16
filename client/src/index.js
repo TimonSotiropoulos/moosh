f4aa69538f66012c29e0b688dc91f4c51f98ad1e
TimonSotiropoulos <timon.sotiropoulos@gmail.com>
2018-05-16 11:05:56 +1000

    Added sub folders and redux/ruter to work with application

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'regenerator-runtime/runtime';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import { store, history } from './store/createStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Add the below lines if redux persistance is required
+// import { store, persistor, history } from './store/createStore'
+// import { PersistGate } from 'redux-persist/integration/react'
+
+import App from './pages/_App';
+
+import 'sanitize.css/sanitize.css'
+import './index.css'
+
+const rootDiv = document.querySelector('#root');
+
+ReactDOM.render((
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div>
+                <App />
+            </div>
+        </ConnectedRouter>
+    </Provider>
+), rootDiv)
---
+++ client/src/pages/_App.js
@@ -0,0 +1,33 @@
+// *******************************************************
+// Main Application Container
+// -------------------------------------------------------
+// The is the mia entry point of the application. Here is where
+// all the base files are introduced and the router is held
+// within this file as well.
+// -------------------------------------------
+
+// *******************************************
+// Module Imports
+// -------------------------------------------
+import React, { Component } from 'react';
+import { withRouter } from 'react-router'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+// --------------------------------
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
