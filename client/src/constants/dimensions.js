171a50205eca415ed4fe56a253657c8a3cdc95ed
TimonSotiropoulos <timon.sotiropoulos@gmail.com>
2018-05-25 17:34:41 +1000

    Added hopeful fix to resolve window sizing issues

--- client/src/constants/dimensions.js
+++ client/src/constants/dimensions.js
@@ -7,6 +7,12 @@ export const SVG_VIEW_WIDTH = 2048;
 export const SVG_VIEW_HEIGHT = 1536;
 // export const APP_WIDTH = RAW_APP_WIDTH / 2;
 // export const APP_HEIGHT = RAW_APP_HEIGHT / 2;
-export const APP_WIDTH = "100vw";
-export const APP_HEIGHT = "100vh";
+// export const APP_WIDTH = "100%";
+/* eslint-disable */
+console.log(screen.availHeight);
+console.log(window.innerWidth);
+
+export const APP_WIDTH = window.innerWidth;
+export const APP_HEIGHT = window.innerHeight;
+/* eslint-enable */
 // -------------------------------------------
